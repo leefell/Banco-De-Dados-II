ef365ae12da95c58a1227d53312f843fa2a539fc
Alexandre <129619218+leefell@users.noreply.github.com>
2024-09-15 18:17:37 -0300

    Update exercicioCriacaoTabelas.sql

--- Exercícios/exercicioCriacaoTabelas.sql
+++ Exercícios/exercicioCriacaoTabelas.sql
@@ -19,8 +19,8 @@ CREATE TABLE atorEstrelaFilme
 (
     idAtor INTEGER,
     codFilme INTEGER,
-    CONSTRAINT fk_idAtor FOREIGN KEY(idAtor) REFERENCES ATOR,
-    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME,
+    CONSTRAINT fk_idAtor FOREIGN KEY(idAtor) REFERENCES ATOR(idAtor),
+    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME(codFilme),
     CONSTRAINT pk_atorEstrelaFilme PRIMARY KEY(idAtor, codFilme)
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE EXEMPLAR
 (
     nroExemplar INTEGER,
     codFilme INTEGER,
-    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME,
+    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME(codFilme),
     CONSTRAINT pk_exemplar PRIMARY KEY(nroExemplar, codFilme)
 );
 
@@ -48,7 +48,7 @@ CREATE TABLE TelefoneCliente
 (
     nroTelefone INTEGER,
     codCliente INTEGER,
-    CONSTRAINT fk_codCliente FOREIGN KEY(codCliente) REFERENCES CLIENTE,
+    CONSTRAINT fk_codCliente FOREIGN KEY(codCliente) REFERENCES CLIENTE(codCliente),
     CONSTRAINT pk_TelefoneCliente PRIMARY KEY(nroTelefone, codCliente)
 );
 
@@ -59,7 +59,47 @@ CREATE TABLE CLIENTE_has_EXEMPLAR
     nroExemplar INTEGER,
     dtaEmpr DATE,
     dtaDev DATE,
-    CONSTRAINT fk_nroExemplar FOREIGN KEY(nroExemplar, codFilme) REFERENCES EXEMPLAR,
-    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME,
+    CONSTRAINT fk_nroExemplar FOREIGN KEY(nroExemplar, codFilme) REFERENCES EXEMPLAR(nroExemplar, codFilme),
+    CONSTRAINT fk_codFilme FOREIGN KEY(codFilme) REFERENCES FILME(codFilme),
     CONSTRAINT pk_CLIENTE_has_EXEMPLAR PRIMARY KEY(nroExemplar, codFilme, codCliente)
+);
+
+CREATE TABLE proprietario
+(
+    cod_propri INTEGER,
+    nome VARCHAR(30),
+    CONSTRAINT pk_proprietario PRIMARY KEY(cod_propri)
+);
+
+CREATE TABLE fisico
+(
+    cod_propri INTEGER,
+    CPF INTEGER UNIQUE,
+    CONSTRAINT fk_proprietario FOREIGN KEY(cod_propri) REFERENCES proprietario(cod_propri),
+    CONSTRAINT pk_fisico PRIMARY KEY(cod_propri, CPF)
+);
+
+CREATE TABLE juridico
+(
+    cod_propri INTEGER,
+    CNPJ INTEGER UNIQUE,
+    CONSTRAINT fk_proprietario FOREIGN KEY(cod_propri) REFERENCES proprietario(cod_propri),
+    CONSTRAINT pk_juridico PRIMARY KEY(cod_propri, CNPJ)
+);
+
+CREATE TABLE veiculo
+(
+    placa VARCHAR(7),
+    modelo VARCHAR(30),
+    ano_fabricacao INTEGER,
+    CONSTRAINT pk_veiculo PRIMARY KEY(placa)
+);
+
+CREATE TABLE possui
+(
+    cod_propri INTEGER,
+    placa VARCHAR(7),
+    CONSTRAINT fk_proprietario FOREIGN KEY(cod_propri) REFERENCES proprietario(cod_propri),
+    CONSTRAINT fk_placa FOREIGN KEY(placa) REFERENCES veiculo(placa),
+    CONSTRAINT pk_possui PRIMARY KEY(cod_propri, placa)
 );
